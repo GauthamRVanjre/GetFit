faf00070174b8f2f54609bf7c8849f61818123ba
Gautham Vanjre <vanjregautham@gmail.com>
2023-06-11 14:10:16 +0530

    added navigation paths

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./screens/Home";
+import SplashScreen from "./screens/SplashScreen";
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Splash Screen" component={SplashScreen} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
