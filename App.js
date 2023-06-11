f20d6391a0df89b8477b94e6b55c2c3f31c04a83
Gautham Vanjre <vanjregautham@gmail.com>
2023-06-11 14:06:55 +0530

    added navigation dependencies

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Home from "./screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aye Vedyaa! Bat ka grip</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: 750,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 250,
   },
 });
